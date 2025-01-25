478428e30e35c3f713a076dbbf6fca24335fe1fb
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-01-24 19:45:29 -0800

    removed /env from repo, changed vite config for Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/",
+	build: {
+		outDir: "dist",
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
