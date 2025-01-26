55eb7cbb28badba76f8e61b4250eb09b61ad0395
Jorge Buenrostro <jlbmontejano@gmail.com>
2025-01-26 13:15:59 -0800

    Moved logout function to context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,26 +1,39 @@
+import { toast } from "@/hooks/use-toast";
 import { User } from "@/types";
 import { createContext, ReactNode, useContext, useState } from "react";
+import { useNavigate } from "react-router";
 
 type AuthContext = {
 	user: User | null;
 	setUser: React.Dispatch<React.SetStateAction<User | null>>;
+	handleLogOut: (message: string) => void;
 };
 
-const INITIAL_STATE = {
-	user: null,
-	setUser: () => {},
-};
-
-const AuthContext = createContext<AuthContext>(INITIAL_STATE);
+const AuthContext = createContext<AuthContext | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
 	const [user, setUser] = useState<User | null>(null);
+	const navigate = useNavigate();
+
+	const handleLogOut = (message: string) => {
+		toast({ description: message });
+		setUser(null);
+		navigate("/");
+	};
+
 	const value = {
 		user,
 		setUser,
+		handleLogOut,
 	};
 
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export const useAuthContext = () => useContext(AuthContext);
+export const useAuthContext = () => {
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error("useAuthContext must be used within an AuthProvider");
+	}
+	return context;
+};
